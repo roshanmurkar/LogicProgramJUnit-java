bcafa932ccf73e6046b8dc35cb2dd3746f26c98b
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-10 19:26:49 +0530

    DayofWeek

---
+++ main/DayOfWeek.java
@@ -0,0 +1,18 @@
+package main;
+
+import main.utility.UtilityForDayOfWeek;
+
+public class DayOfWeek {
+	public static void main(String[] args) {
+		
+		double y=0,m=0,d=0;
+		
+		d = Integer. parseInt(args[0]);
+		m = Integer. parseInt(args[1]);		
+		y = Integer. parseInt(args[2]);
+		UtilityForDayOfWeek.getday(d,m,y);
+		UtilityForDayOfWeek.getday(d,m,y);
+		
+		
+	}
+}
---
+++ main/utility/UtilityForDayOfWeek.java
@@ -0,0 +1,52 @@
+package main.utility;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UtilityForDayOfWeek {
+	
+	public final static Logger logger=Logger.getLogger("UtilityForDayOfWeek");
+	
+	public static void getday(int d,int m,int y) {
+		float x=0;
+		y=(y-(14-m))/12;
+		x=y+(y/4)-(y/100)+(y/400);
+		m=m+(12*((14-m)/12))-2;
+		d=(int) ((d+x+((31*m)/12))%7);
+		
+		switch (d) {
+		case 0: {
+			logger.log(Level.INFO,"sunday");
+			break;
+		}
+		case 1: {
+			logger.log(Level.INFO,"MONDAY");
+			break;
+		}
+		case 2: {
+			logger.log(Level.INFO,"TUESDAY");
+			break;
+		}
+		case 3: {
+			logger.log(Level.INFO,"WEDNESDAY");
+			break;
+		}
+		case 4: {
+			logger.log(Level.INFO,"THURSDAY");
+			break;
+		}
+		case 5: {
+			logger.log(Level.INFO,"FRIDAY");
+			break;
+		}
+		case 6: {
+			logger.log(Level.INFO,"SATURDAY");
+			break;
+		}
+		
+		default:
+			logger.log(Level.INFO,"Wrong Input");
+		}
+	}
+}

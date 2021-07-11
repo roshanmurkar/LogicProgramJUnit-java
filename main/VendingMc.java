0d5fe2893dfb26593cbfe0c1e6f4481bb7c451ab
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-11 11:25:42 +0530

    update

--- main/VendingMc.java
+++ main/VendingMc.java
@@ -1,22 +1,10 @@
 package main;
 
-import java.util.logging.Logger;
-
-import main.utility.UVendingMc;
-
 public class VendingMc {
-	public final static Logger logger=Logger.getLogger("VendingMachine");
+
 	public static void main(String[] args) {
-		int[] arrayOfNotes=new int[8];
-		
-		
-		
-		//getting input from user
-		int getInput=UVendingMc.getUserInput();
-		
-		//checking for change from machine
-		UVendingMc.change(getInput);
-		
-		
+		// TODO Auto-generated method stub
+
 	}
+
 }
--- main/utility/UVendingMc.java
+++ main/utility/UVendingMc.java
@@ -1,21 +1,5 @@
 package main.utility;
 
-import java.util.Scanner;
-
 public class UVendingMc {
-	
-	public static int[] arrayOfNotes=new int[8];
-	
-	
-	public static int getUserInput() {
-	
-		Scanner scan = new Scanner(System.in);
-		int getUserInput = scan.nextInt();
-		
-		return getUserInput; 
-	}
-	
-	public static void change(int getInput) {
-		if(getInput<1000)
-	}
+
 }
